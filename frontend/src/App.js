890244954d69c7e2489fd6095237ccca5b001d40
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2022-10-07 18:53:44 +0100

    Menu Slide

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from "react";
+import HomeView from './components/Home/HomeView';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/home" element={< />} /> */}
+        <Route path="/home" element={< HomeView/>} />
+        <Route path="/" element={< HomeView/>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/Home/HomeView.js
@@ -0,0 +1,9 @@
+import SideMenu from "../Global/SideMenu";
+
+export default function HomeView(){
+    return(
+        <>
+        <SideMenu></SideMenu>
+        </>
+    )
+}
