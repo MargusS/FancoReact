97cd2d08d7dbe34f8460a89f86f3eec32e22d076
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2022-10-28 16:58:15 +0100

    Logged View

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import HomeView from './components/Home/HomeView';
 import LoginView from './components/Login/LoginView';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoggedView from './components/Logged/LoggedView';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/home" element={< HomeView />} />
         <Route path="/login" element={< LoginView />} />
+        <Route path="/logged" element={< LoggedView />} />
         <Route path="/" element={< HomeView />} />
       </Routes>
     </Router>
--- frontend/src/components/Login/LoginView.js
+++ frontend/src/components/Login/LoginView.js
@@ -2,8 +2,11 @@ import SideMenu from "../Global/SideMenu";
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 import Footer from "../Global/Footer";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginView() {
+
+    const navigate = useNavigate();
 
     const onFinish = (values) => {
         console.log('Success:', values);
@@ -25,29 +28,22 @@ export default function LoginView() {
                 <Button className="login-options-button" type="primary" danger size="large">Login with Google <GoogleOutlined /></Button>
                 <Button className="login-options-button" type="primary" size="large">Login with Facebook<FacebookOutlined /></Button>
             </div>
-            <div className="login-options">
+            <hr className="divider"></hr>
+            <div className="login-options login-form">
                 <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
-                    <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
-                    >
+                    <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
                         <Input />
                     </Form.Item>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                    >
+                    <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
                         <Input.Password />
                     </Form.Item>
-                    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ span: 16 }}>
                         <Checkbox>Remember me</Checkbox>
                     </Form.Item>
 
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                        <Button type="primary" htmlType="submit">
+                    <Form.Item wrapperCol={{ offset: 1, span: 10 }}>
+                        <Button className="submit-btn" type="primary" htmlType="submit" size="large" onClick={() => navigate("/logged")}>
                             Submit
                         </Button>
                     </Form.Item>
